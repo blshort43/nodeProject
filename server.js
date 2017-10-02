5be44872fdb7fd7ea4ea35a69ed05b5785dbe242
Brandon <blshort43@gmail.com>
2017-10-01 21:21:07 -0500

    created all CRUD routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 const app = express();
 
@@ -9,9 +10,11 @@ const port = 8000;
 //body parser to process URL encoded forms
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//import routes
-require('./app/routes')(app, {});
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err)
+    require('./app/routes')(app, database);
 
-app.listen(port, () => {
-	console.log('Connected on port ' + port);
-});
+    app.listen(port, () => {
+        console.log('Connected on port ' + port);
+    });
+})
